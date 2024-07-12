16e378e95ca78996f68bbacdd1b4ed24d2d4d898
dorian <doriantheblue@naver.com>
2024-07-12 21:30:58 +0900

    refactor: add spanish

--- hello_test.go
+++ hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Harok")
+		got := Hello("Harok", "English")
 		want := "Hello, Harok"
 
 		if got != want {
@@ -12,12 +12,18 @@ func TestHello(t *testing.T) {
 		}
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "English")
 		want := "Hello, World"
 
 		if got != want {
 			assertCorrectMessage(t, got, want)
 		}
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
 	})
 }
 
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 const englishPrefixHello = "Hello, "
+const spanishPrefixHello = "Hola, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return spanishPrefixHello + name
 	}
 
 	return englishPrefixHello + name
