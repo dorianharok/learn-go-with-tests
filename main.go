ecc46d9840de9a6e158a9c8b3199a9d71c9439ef
dorian <doriantheblue@naver.com>
2024-07-12 21:47:28 +0900

    feat: greetingPrefix

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
-const englishPrefixHello = "Hello, "
-const spanishPrefixHello = "Hola, "
-const frenchPrefixHello = "Bonjour, "
+const (
+	spanish = "Spanish"
+	french  = "French"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishPrefixHello
+	return greetingPrefix(language) + name
+}
 
+func greetingPrefix(language string) (prefix string) {
 	switch language {
-	case "Spanish":
-		prefix = spanishPrefixHello
-	case "French":
-		prefix = frenchPrefixHello
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-
-	return prefix + name
+	return
 }
 
 func main() {
